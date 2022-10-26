baa1151b1359027fdb2febdf31ae03fa8dd862cd
Gaurav <grvjain56@gmail.com>
2022-10-26 14:21:00 +0530

    first commit

---
+++ movie/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react' ; // imrc
+
+class App extends Component { // cc  
+  state = {  } ;
+  render() { 
+    return <div className="App"> Hello From App </div> ;
+  }
+}
+ 
+export default App;
+
+
