b4f67782213b1502355f67d6d7da6c213a747e86
Gaurav <grvjain56@gmail.com>
2022-10-26 18:24:19 +0530

    Movies Data Rendered

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react' ; // imrc
+import Header from "./Components/Header/Header.jsx" ;
+import Movies from './Components/Movies/Movies.jsx';
+import axios from "axios" ;
+import { API_KEY , API_URL , IMAGE_URL } from "./API/secrets.js"
 
 class App extends Component { // cc  
-  state = {  } ;
+  state = {   
+    moviesData:[],
+    currentMovie: "avengers",
+   } ;
+  
+  async componentDidMount()
+  {
+    // API call
+    // params => api key , page , query
+    // https://api.themoviedb.org/3/search/movie?api_key=bdd243ea847239dc0799805e63e189f0&query=avengers&page=1&include_adult=false
+  
+    let data = await axios.get(API_URL + "/search/movie" , {
+      params: {api_key : API_KEY , page: 1 , query: this.state.currentMovie},
+    }) ;
+    
+    // console.log(data) ;
+    let moviesData = data.data.results ;
+    console.log(moviesData) ;
+    this.setState({
+      moviesData:moviesData
+    })
+  
+  
+  }
+
   render() { 
-    return <div className="App"> Hello From App </div> ;
+    return <div className="App"> 
+      <Header></Header>
+      <Movies movies={this.state.moviesData}></Movies>   
+    </div> ; 
   }
 }
  
